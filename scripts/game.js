0916df2ec5e0bd6bda63cb7d5d06b802a5e698f0
Nico-mint <nicolasnerfo@gmail.com>
2024-12-12 00:40:04 +0100

    added yellow transparent square over the squares the cursor is in

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,3 @@
-
 export class Game {
 	constructor() {
 		this.board = [];
@@ -6,10 +5,34 @@ export class Game {
 		this.turn = "white";
 	}
 
+	// Shows a transparent yellow square over the square the curor is in
+	handleMouseOver(event) {
+		const overlay = event.target.querySelector('.overlay');
+		if (overlay) {
+			overlay.style.display = 'block';
+
+			const squareId = event.currentTarget.id;
+			console.log(`Square ID: ${squareId}`);
+		}
+	}
+
+	// Once the cursor is not in the square, the transparent square disappears
+	handleMouseOut(event) {
+		const overlay = event.target.querySelector('.overlay');
+		if (overlay) {
+			overlay.style.display = 'none';
+		}
+	}
+
 	startGame() {
-		// Add initial pieces
-		// 
-		this.board = [];
+		console.log(`CHESS GAME STARTED`);
+		const divs = document.querySelectorAll('.cell');
+
+		// Add the events to all the board squares
+		divs.forEach(div => {
+			div.addEventListener('mouseover', this.handleMouseOver.bind(this));
+			div.addEventListener('mouseout', this.handleMouseOut.bind(this));
+		});
 	}
 
 	handleMove(start, end) {
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 import { Game } from "./game.js";
 
+
 const game = new Game();
 
-document.getElementById("restart").addEventListener("click", () => {
-	game.startGame();
-});
+// document.getElementById("restart").addEventListener("click", () => {
+// 	game.startGame();
+// });
 
 game.startGame();
